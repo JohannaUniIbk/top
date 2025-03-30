cc7d1cc7b282cdf9c2bb4c15a26d7c43fdfd4eae
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-30 17:56:14 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 Script für die Lieblingsorte
 */
 //Karte initialisieren
-var map = L.map('map').setView([49.28098, -123.12244], 11);
+let map = L.map('map').setView([49.28098, -123.12244], 11);
 //Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,6 +10,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //MArker zeichnen
 }).addTo(map);
-var marker = L.marker([49.28098, -123.12244]).addTo(map);
+let marker = L.marker([49.28098, -123.12244]).addTo(map);
  //Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>This is Vancouver").openPopup();
