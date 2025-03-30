e70d37e02fb0dc327cab74ad64d76a4d6d13160b
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-30 18:04:19 +0200

    let stop definiert und Popup bearbeitet

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ Script für die Lieblingsorte
 let lat = 49.28098;
 let lng = -123.12244;
 let zoom = 11;
+
+let stop = {
+    nr.: 17,
+    title: Vancouver,
+    user: johannauniibk,
+    lat: 49.28098,
+    lng: -123.12244,
+    zoom: 11,
+}
+
 //Karte initialisieren
 let map = L.map('map').setView([lat, lng], zoom);
 //Hintergrundkarte definieren
@@ -16,7 +26,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let marker = L.marker([lat, lng]).addTo(map);
  //Popup definieren und öffnen
 marker.bindPopup(`
-    <b>Hello world!</b>
-    <br>
-    This is Vancouver at ${lat.toFixed(5)} / ${lng.toFixed(5)}.
+    <h2>Vancouver</h2>
+            <ul>
+                <li>Geogr. Breite: ${lat.toFixed(5)}° </li>
+                <li>Geogr. Länge: ${lng.toFixed(5)}°</li>
+            </ul>
     `).openPopup();
