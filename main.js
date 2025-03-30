aa520117c1a07f70ff5b67c89e8d86398589a3d5
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-30 18:18:24 +0200

    Etappe als erstes im Pulldown

--- main.js
+++ main.js
@@ -231,3 +231,15 @@ marker.bindPopup(`
         console.log(STOPS[i].user, "meine Etappe:-)");
         map.setView([STOPS[i].lat, STOPS[i].lng], STOPS[i].zoom);
     marker.openPopup();
+}
+//Pulldown Menü befüllen
+let option = document.createElement("option");
+option.value = STOPS[i].user;
+option.text = STOPS[i].title;
+
+if (STOPS[i].user == "johannauniibk"){
+    option.selected = true;
+}
+document.querySelector("#pulldown select").appendChild(option);
+
+}
