7a34b38a0326e0800ece5c3c8d717f0e1566125b
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-30 17:57:59 +0200

    Variablen für lat, lng und zoom gesetzt

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 /*
 Script für die Lieblingsorte
 */
+let lat = 49.28098;
+let lng = -123.12244;
+let zoom = 11;
 //Karte initialisieren
-let map = L.map('map').setView([49.28098, -123.12244], 11);
+let map = L.map('map').setView([lat, lng], zoom);
 //Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,6 +13,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //MArker zeichnen
 }).addTo(map);
-let marker = L.marker([49.28098, -123.12244]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
  //Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>This is Vancouver").openPopup();
